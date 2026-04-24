8d727df4e1711933062441e737ca3256a1f4cd7e
Manthan B T <manthan.bt@gmail.com>
2026-04-24 16:15:33 +0530

    chore: remove project configuration and documentation files

--- packages/shared/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './game-engine.js';
-export * from './bot-engine.js';
-export * from './encryption-service.js';
-export * from './mmr-service.js';
-export * from './moderation-service.js';
